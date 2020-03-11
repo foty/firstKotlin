61940cbabf7503e26bb292ba2eb769bf11e38769
ccj <997441082@qq.com>
2020-03-11 18:19:42 +0800

    添加项目模块内容

---
+++ app/src/main/java/com/example/foty/mykotlin/project/ProjectFragment.kt
@@ -0,0 +1,33 @@
+package com.example.foty.mykotlin.project
+
+import com.example.foty.mykotlin.base.BaseMvpFragment
+
+/**
+ * Create by lxx
+ * Date : 2020/3/11 17:35
+ * Use by
+ */
+class ProjectFragment : BaseMvpFragment<ProjectPresenter>(), ProjectConstract.View {
+
+    companion object{
+        fun newInstance() = ProjectFragment()
+    }
+
+
+    override fun initPresenter(): ProjectPresenter {
+        return ProjectPresenter(this)
+    }
+
+    override fun getLayoutResID(): Int {
+        return 0
+    }
+
+    override fun initData() {
+    }
+
+    override fun initView() {
+    }
+
+    override fun initLoad() {
+    }
+}
---
+++ app/src/main/java/com/example/foty/mykotlin/project/ProjectPresenter.kt
@@ -0,0 +1,11 @@
+package com.example.foty.mykotlin.project
+
+import com.example.foty.mykotlin.base.BasePresenter
+
+/**
+ * Create by lxx
+ * Date : 2020/3/11 17:36
+ * Use by
+ */
+class ProjectPresenter(view: ProjectConstract.View) : BasePresenter<ProjectConstract.View>(view) {
+}
